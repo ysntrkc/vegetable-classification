b10ccc5d0c3da15cc5c311640f164fd83fc3aea2
ysntrkc <yasin.trkc.142@gmail.com>
2023-01-10 01:40:28 +0300

    fix

--- Main_Page_🏡.py
+++ Main_Page_🏡.py
@@ -65,7 +65,6 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
     st.write("")
-    st.write("Classifying...")
 
     cnn_result = make_prediction(cnn_model, image)
     resnet_result = make_prediction(resnet_model, image)

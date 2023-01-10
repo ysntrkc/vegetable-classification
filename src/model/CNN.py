177d8c9d383706525ec26010f8dd14e9d2ac76c7
yasintrkc <yasin.142.tarakci@gmail.com>
2023-01-10 22:06:36 +0300

    Revert "chnage cnn layers"
    This reverts commit e6b9b1b809cce2dcf123e75a0cb551a91a80b05a.

--- src/model/CNN.py
+++ src/model/CNN.py
@@ -1,31 +1,48 @@
+import torch
 import torch.nn as nn
 
 
 class CNN(nn.Module):
-    def __init__(self, num_classes=15):
-        super(CNN, self).__init__()
-
-        # Using Sequential container
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(128, 192, kernel_size=3, padding=1, stride=1),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.Flatten(),
+            nn.MaxPool2d(2, 2)
         )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(37 * 37 * 64, 128),
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(192, 256, kernel_size=3, padding=1, stride=1),
             nn.ReLU(),
-            nn.Dropout(0.25),
-            nn.Linear(128, 128),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, stride=1),
             nn.ReLU(),
-            nn.Linear(128, 15),
+            nn.MaxPool2d(2, 2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(512 * 18 * 18, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(32, num_classes)
         )
 
     def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
         return x
